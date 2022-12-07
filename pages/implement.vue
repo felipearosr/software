adf22eaa3324d4abb51a3f5bc80546f3f39b6a16
Vicente <zxv1ch0xz@gmail.com>
2022-12-07 10:40:29 -0300

    Moved the import functionality to the import page, added the export functionality to the implementation page, also added a couple packages to parse and generate csv files, also a third module to download files. OwO

--- pages/implement.vue
+++ pages/implement.vue
@@ -1,40 +1,75 @@
 <script>
 import axios from 'axios';
 import Papa from 'papaparse';
+// export default {
+//   methods: {
+//     onFileChange() {
+//       const supabase = useSupabaseClient()
+
+//       const file = this.$refs.csvFileInput.files[0];
+
+//       const reader = new FileReader();
+
+//       reader.onload = async (event) => {
+//         const csv = event.target.result;
+//         const data = Papa.parse(csv);
+//         const arr = data.data.map((row) => [...row]);
+
+//         for (let i = 0; i < arr.length-1; i++) {
+          
+//           const { error } = await supabase.from('Product').insert({id: arr[i][0], name: arr[i][1], price: arr[i][2], description: arr[i][3]})
+
+//         }
+
+//       };
+
+//       reader.readAsText(file);
+//     },
+//   },
+// };
+import { Parser } from 'json-2-csv';
+import FileSaver from 'file-saver';
+
 export default {
   methods: {
-    onFileChange() {
+    async downloadCSV() {
+      // Get the JSON data from the API
       const supabase = useSupabaseClient()
+      const { data, error } = await supabase
+        .from('Product')
+        .select()
+        .csv()
+        console.log(data)
+        // axios.get('/api/data')
+        // .then((response) => {
+          // Convert the JSON data to CSV format
+          //const parser = new Parser();
+          const csv = data;
 
-      const file = this.$refs.csvFileInput.files[0];
+          // Create a Blob object with the CSV data
+          const blob = new Blob([csv], { type: 'text/csv;charset=utf-8' });
 
-      const reader = new FileReader();
-
-      reader.onload = async (event) => {
-        const csv = event.target.result;
-        const data = Papa.parse(csv);
-        const arr = data.data.map((row) => [...row]);
-
-        for (let i = 0; i < arr.length-1; i++) {
-          
-          const { error } = await supabase.from('Product').insert({id: arr[i][0], name: arr[i][1], price: arr[i][2], description: arr[i][3]})
-
-        }
-
-      };
-
-      reader.readAsText(file);
+          // Download the CSV file
+          FileSaver.saveAs(blob, 'data.csv');
+        // })
+        // .catch((error) => {
+        //   console.error(error);
+        // });
+        
     },
   },
 };
-
-
-
 </script>
 
 <script setup>
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
+
+
+
+
+// console.log(data)
+
 const { data: products } = await useAsyncData(async () => {
   const { data } = await supabase
     .from('Product')
@@ -42,12 +77,12 @@ const { data: products } = await useAsyncData(async () => {
   return data
 })
 
-const { data, error } = await supabase.rpc('get_pd_wh').eq('wid', 1)
-console.log(data)
-// const { data, error } = await supabase.rpc('get_pd_name', {var_id: 1})
+// const { data, error } = await supabase.rpc('get_pd_wh').eq('wid', 1)
+// console.log(data)
 </script>
 <template>
-    <input type="file" ref="csvFileInput" @change="onFileChange">
+    <button @click="downloadCSV">Download CSV</button>
+    <!-- <input type="file" ref="csvFileInput" @change="onFileChange"> -->
 
     <div>        
         <div class = "container mx-auto">
--- pages/import.vue
+++ pages/import.vue
@@ -1,9 +1,37 @@
+<script>
+import axios from 'axios';
+import Papa from 'papaparse';
+export default {
+  methods: {
+    onFileChange() {
+      const supabase = useSupabaseClient()
+
+      const file = this.$refs.csvFileInput.files[0];
+
+      const reader = new FileReader();
+
+      reader.onload = async (event) => {
+        const csv = event.target.result;
+        const data = Papa.parse(csv);
+        const arr = data.data.map((row) => [...row]);
+
+        for (let i = 0; i < arr.length-1; i++) {
+          
+          const { error } = await supabase.from('Product').insert({id: arr[i][0], name: arr[i][1], price: arr[i][2], description: arr[i][3]})
+
+        }
+
+      };
+
+      reader.readAsText(file);
+    },
+  },
+};
+</script>
+
 <template>
+   <input type="file" ref="csvFileInput" @change="onFileChange">
  <div>
     Lame la tula cristobal
  </div>
 </template>
-
-<script setup>
-
-</script>
